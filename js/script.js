1a7e5ebb281a76fe55d962155e46c20bca7c896f
Joan <joanplanasole@gmail.com>
2025-11-08 19:14:21 +0100

    fixs de links + parte contacto nueva

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function scrollToSection(id) {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: 'smooth' });
 }
+
+// Menú hamburguesa para móviles
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
